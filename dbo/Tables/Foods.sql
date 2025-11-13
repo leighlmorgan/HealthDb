b722eff11662e1bbc402b67e1eb06d5afb0e7ffd
Leigh Morgan <leigh@leighmorgan.com>
2025-11-13 18:47:28 +0000

    Add project files.

---
+++ dbo/Tables/Foods.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Foods] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (100) NOT NULL,
+    [Description] NVARCHAR (MAX) NOT NULL, 
+    [Fat] DECIMAL(7, 3) NOT NULL,
+    [FatSaturates] DECIMAL(7, 3) NOT NULL,
+    [Carbohydrate] DECIMAL(7, 3) NOT NULL,
+    [CarbohydrateSugar] DECIMAL(7, 3) NOT NULL,
+    [Fibre] DECIMAL(7, 3) NOT NULL,
+    [Protein] DECIMAL(7, 3) NOT NULL,
+    [Salt] DECIMAL(7, 3) NOT NULL,
+    [Calcium] DECIMAL(7, 3) NOT NULL,
+);
+
